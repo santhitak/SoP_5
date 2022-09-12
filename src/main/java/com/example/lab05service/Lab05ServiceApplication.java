6072b73c97061faf3e85913902ad85a104eca7e4
santhitak <santhita.sk@gmail.com>
2022-09-13 05:22:20 +0700

    init

---
+++ src/main/java/com/example/lab05service/Lab05ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab05service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab05ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab05ServiceApplication.class, args);
+    }
+
+}
